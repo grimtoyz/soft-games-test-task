85a964e8f629a8dea8df445274fb43fb4d6532b4
Vladislav.Druchin <v.druchyn@twinwingames.com>
2026-03-12 18:52:33 +0200

    refactoring and cleaning up

--- src/view/components/fire/Fire.ts
+++ src/view/components/fire/Fire.ts
@@ -1,125 +1,114 @@
 import * as PIXI from 'pixi.js';
-import gsap from "gsap";
-import {FireEmitter} from "./FireEmitter";
-import {Sprite} from "pixi.js";
-import fireSceneConfig from "../../../config/scenes/phoenixFlameConfig.json";
-import {Utils} from "../../../utils/Utils";
+import gsap from 'gsap';
+import { FireEmitter } from './FireEmitter';
+import { Sprite } from 'pixi.js';
+import fireSceneConfig from '../../../config/scenes/phoenixFlameConfig.json';
+import { Utils } from '../../../utils/Utils';
 
 export class Fire extends PIXI.Container {
-	private _app: PIXI.Application;
-	private _glow: PIXI.Sprite;
-	private _fireBackGlow: PIXI.Sprite;
-	private _fire: PIXI.Sprite;
-	private _glowTime: number;
-	private _glowPhase: number;
-	private _glowAmp: number;
-	private _glowBaseAlpha: number;
-	private _glowPauseTimer: number;
-	private _glowTargetAmp: number;
+  private _glow!: PIXI.Sprite;
+  private _fireBackGlow!: PIXI.Sprite;
+  private _fire!: PIXI.Sprite;
+  private _glowTime!: number;
+  private _glowPhase!: number;
+  private _glowAmp!: number;
+  private _glowBaseAlpha!: number;
+  private _glowPauseTimer!: number;
+  private _glowTargetAmp!: number;
+  private _emitter!: FireEmitter;
 
-	constructor(app: PIXI.Application) {
-		super();
+  constructor() {
+    super();
 
-		this._app = app;
+    this._createGlow();
+    this._createFireplace();
+    this._createEmitter();
+  }
 
-		this._createGlow();
-		this._createFireplace();
-		this._createEmitter();
-	}
+  private _createGlow(): void {
+    this._fireBackGlow = Sprite.from('glow_circle.png');
+    this._fireBackGlow.blendMode = 'add';
+    this._fireBackGlow.anchor.set(0.5);
+    this._fireBackGlow.position.y = -400;
+    this._fireBackGlow.scale.set(2);
+    this.addChild(this._fireBackGlow);
 
-	private _createGlow(): void {
-		this._fireBackGlow = Sprite.from('glow_circle.png');
-		this._fireBackGlow.blendMode = 'add';
-		this._fireBackGlow.anchor.set(0.5);
-		this._fireBackGlow.position.y = -400;
-		this._fireBackGlow.scale.set(2);
-		this.addChild(this._fireBackGlow);
+    this._glow = Sprite.from('glow.png');
+    this._glow.blendMode = 'add';
+    this._glow.anchor.set(0.5, 0.4);
 
-		this._glow = Sprite.from('glow.png');
-		this._glow.blendMode = 'add';
-		this._glow.anchor.set(0.5, 0.4);
+    this._glowTime = 0;
+    this._glowPhase = Math.random() * Math.PI * 2;
+    this._glowBaseAlpha = 1;
+    this._glowPauseTimer = Utils.rand(0.08, 0.28);
+    this._glowAmp = Utils.rand(0.03, 0.12);
+    this._glowTargetAmp = Utils.rand(0.03, 0.12);
 
-		this._glowTime = 0;
-		this._glowPhase = Math.random() * Math.PI * 2;
-		this._glowBaseAlpha = 1;
-		this._glowPauseTimer = Utils.rand(0.08, 0.28);
-		this._glowAmp = Utils.rand(0.03, 0.12);
-		this._glowTargetAmp = Utils.rand(0.03, 0.12);
+    this.addChild(this._glow);
+  }
 
-		this.addChild(this._glow);
-	}
+  private _createFireplace(): void {
+    this._fire = Sprite.from('fireplace.png');
+    this._fire.anchor.set(0.5, 0.8);
 
-	private _createFireplace(): void {
-		this._fire = Sprite.from('fireplace.png')
-		this._fire.anchor.set(0.5, 0.8);
+    this.addChild(this._fire);
+  }
 
-		this.addChild(this._fire);
-	}
+  private _createEmitter(): void {
+    const flameTextures = [
+      PIXI.Assets.get('fire_particle_type_b.png'),
+      PIXI.Assets.get('fire_particle_type_c.png'),
+    ];
+    this._emitter = new FireEmitter({
+      flameTextures,
+      smokeTextures: PIXI.Assets.get('smoke_type_a.png'),
 
-	private _createEmitter(): void {
-		const flameTextures = [
-			PIXI.Assets.get('fire_particle_type_b.png'),
-			PIXI.Assets.get('fire_particle_type_c.png'),
-		];
-		const emitter = new FireEmitter({
-			flameTextures,
-			smokeTextures: PIXI.Assets.get('smoke_type_a.png'),
+      maxParticles: fireSceneConfig.emitter.maxParticles,
+      emitEvery: 0.02,
+      smokeChance: 0.1,
 
-			maxParticles: fireSceneConfig.emitter.maxParticles,
-			emitEvery: 0.02,
-			smokeChance: 0.1,
+      baseWind: 2,
+      windGustAmp: 8,
+      windFreq: 1.4,
 
-			baseWind: 2,
-			windGustAmp: 8,
-			windFreq: 1.4,
+      swayAmp: 2.5,
+      swaySpeed: 2.1,
 
-			swayAmp: 2.5,
-			swaySpeed: 2.1,
+      skewAmp: 0.04,
+      skewSpeed: 1.8,
+    });
 
-			skewAmp: 0.04,
-			skewSpeed: 1.8,
-		});
+    this._emitter.scale.set(2.4);
+    this._emitter.position.y = -170;
 
-		emitter.scale.set(2.4);
-		emitter.position.y = -170;
+    this.addChild(this._emitter);
+  }
 
-		this.addChild(emitter);
+  private _updateGlow(dt: number): void {
+    this._glowTime += dt;
+    this._glowPauseTimer -= dt;
 
-		this._app.ticker.add((ticker) => {
-			emitter.update(ticker.deltaMS / 1000);
-		});
-	}
+    if (this._glowPauseTimer <= 0) {
+      this._glowPauseTimer = Utils.rand(0.08, 0.28);
 
-	private _updateGlow(dt: number): void {
-		this._glowTime += dt;
-		this._glowPauseTimer -= dt;
+      this._glowTargetAmp = Math.random() < 0.25 ? Utils.rand(0.0, 0.02) : Utils.rand(0.03, 0.12);
+    }
 
-		if (this._glowPauseTimer <= 0) {
-			this._glowPauseTimer = Utils.rand(0.08, 0.28);
+    this._glowAmp = Utils.lerp(this._glowAmp, this._glowTargetAmp, 0.08);
 
-			this._glowTargetAmp = Math.random() < 0.25
-				? Utils.rand(0.0, 0.02)
-				: Utils.rand(0.03, 0.12);
-		}
+    const wave1 = Math.sin(this._glowTime * 8.5 + this._glowPhase);
+    const wave2 = Math.sin(this._glowTime * 4.2 + this._glowPhase * 1.7);
+    const wave = wave1 * 0.75 + wave2 * 0.25;
 
-		this._glowAmp = Utils.lerp(this._glowAmp, this._glowTargetAmp, 0.08);
+    this._glow.alpha = gsap.utils.clamp(0.01, 0.3, this._glowBaseAlpha + wave * this._glowAmp);
+    this._fireBackGlow.alpha = this._glow.alpha;
 
-		const wave1 = Math.sin(this._glowTime * 8.5 + this._glowPhase);
-		const wave2 = Math.sin(this._glowTime * 4.2 + this._glowPhase * 1.7);
-		const wave = wave1 * 0.75 + wave2 * 0.25;
+    const s = Utils.lerp(0.98, 1.04, (wave1 + 1) * 0.5);
+    this._glow.scale.set(1.2 * s, s);
+  }
 
-		this._glow.alpha = gsap.utils.clamp(
-			0.01,
-			0.3,
-			this._glowBaseAlpha + wave * this._glowAmp,
-		);
-		this._fireBackGlow.alpha = this._glow.alpha;
-
-		const s = Utils.lerp(0.98, 1.04, (wave1 + 1) * 0.5);
-		this._glow.scale.set(1.2 * s, s);
-	}
-
-	public update(dt: number): void {
-		this._updateGlow(dt);
-	}
+  public update(dt: number): void {
+    this._updateGlow(dt);
+    this._emitter.update(dt);
+  }
 }
--- src/view/scenes/FireScene.ts
+++ src/view/scenes/FireScene.ts
@@ -1,50 +1,43 @@
 import * as PIXI from 'pixi.js';
-import fireSceneConfig from "../../config/scenes/phoenixFlameConfig.json";
-import {GameScene} from "./GameScene";
-import {Sprite} from 'pixi.js';
-import {Fire} from "../components/fire/Fire";
+import fireSceneConfig from '../../config/scenes/phoenixFlameConfig.json';
+import { GameScene } from './GameScene';
+import { Sprite } from 'pixi.js';
+import { Fire } from '../components/fire/Fire';
+import type { GameServices } from '../../game.ts';
 
-export class FireScene extends GameScene{
-	private readonly _app: PIXI.Application;
-	private readonly _bg: PIXI.Sprite;
-	private _fire: Fire;
+export class FireScene extends GameScene {
+  private readonly _app: PIXI.Application;
+  private readonly _bg: PIXI.Sprite;
+  private _fire!: Fire;
 
-	constructor(name: string, app: PIXI.Application) {
-		super(name);
+  constructor(name: string, gameServices: GameServices) {
+    super(name);
 
-		this._app = app;
+    this._app = gameServices.app;
 
-		this._bg = Sprite.from('brown_bg');
-		this._bg.anchor.set(0.5);
-		this._bg.scale.set(2);
+    this._bg = Sprite.from('brown_bg');
+    this._bg.anchor.set(0.5);
+    this._bg.scale.set(2);
 
-		this.addChild(this._bg);
-		this._createFire();
-	}
+    this.addChild(this._bg);
+    this._createFire();
+  }
 
-	public _createFire(): void {
-		this._fire = new Fire(this._app);
-		this.addChild(this._fire)
+  public _createFire(): void {
+    this._fire = new Fire(this._app);
+    this.addChild(this._fire);
 
-		const {x,y} = fireSceneConfig.fireplace.position;
-		this._fire.position.set(x,y);
-	}
+    const { x, y } = fireSceneConfig.fireplace.position;
+    this._fire.position.set(x, y);
+  }
 
-	public onEnter(): void {
-		this._ticker = (ticker) => {
-			this._fire.update(ticker.deltaMS / 1000);
-		};
+  public onEnter(): void {}
 
-		this._app.ticker.add(this._emitterTicker);
-	}
+  public onExit(): void {}
 
-	public onExit(): void {
-	}
+  public update(dt: number): void {
+    this._fire.update(dt);
+  }
 
-	public update(dt: number): void {
-		this._fire.update(dt);
-	}
-
-	public onResize(isPortrait: boolean): void {
-	}
+  public onResize(): void {}
 }
