60a609e77e609614a34afb83297138a9daa552a4
Vladislav.Druchin <v.druchyn@twinwingames.com>
2026-03-13 01:59:37 +0200

    cleaning up

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,8 @@ export const CONSTANTS = {
   EVENTS: {
     RESIZE: 'EVENTS:RESIZE',
   },
+  CARDS: {
+    VALUES: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+    SUITS: ['C', 'D', 'H', 'D'],
+  },
 };
--- src/view/components/cards/Deck.ts
+++ src/view/components/cards/Deck.ts
@@ -1,59 +1,59 @@
 import * as PIXI from 'pixi.js';
-import {Card} from "./Card";
-import aceOfShadows from "../../../config/scenes/aceOfShadowsConfig.json";
-
+import { Card } from './Card';
+import { Utils } from '../../../utils/Utils.ts';
+import { CONSTANTS } from '../../../constants/constants.ts';
 
 export class Deck extends PIXI.Container {
-	private _cards: Card[];
+  private _cards: Card[];
 
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this._cards = [];
-	}
+    this._cards = [];
+  }
 
-	public getNextPosition(): PIXI.Point {
-		const x = this.children.length * 0.2;
-		const y = -this.children.length * 0.2;
+  public getNextPosition(): PIXI.Point {
+    const x = this.children.length * 0.2;
+    const y = -this.children.length * 0.2;
 
-		return new PIXI.Point(x, y);
-	}
+    return new PIXI.Point(x, y);
+  }
 
-	public addCard(card: Card): void {
-		this._cards.push(card);
-		if (card.parent !== this) {
-			this.reparentChild(card);
-		}
-	}
+  public addCard(card: Card): void {
+    this._cards.push(card);
+    if (card.parent !== this) {
+      this.reparentChild(card);
+    }
+  }
 
-	public createCards(amount: number): void {
-		const { cardTextureNames } = aceOfShadows;
+  public createCards(amount: number): void {
+    for (let i = 0; i < amount; i++) {
+      const value = Utils.randomFrom(CONSTANTS.CARDS.VALUES);
+      const suit = Utils.randomFrom(CONSTANTS.CARDS.SUITS);
+      const textureName = `${value}${suit}.png`;
+      const card = new Card(textureName);
+      this.addChild(card);
+      card.x = i * 0.2;
+      card.y = -i * 0.2;
+      card.angle = -1 + Math.random() * 2;
 
-		for (let i = 0; i < amount; i++) {
-			const textureName = cardTextureNames[Math.floor(Math.random() * cardTextureNames.length)];
-			const card = new Card(textureName);
-			this.addChild(card);
-			card.x = i * 0.2;
-			card.y = -i * 0.2;
-			card.angle = -1 + Math.random() * 2;
+      this._cards.push(card);
+    }
+  }
 
-			this._cards.push(card);
-		}
-	}
+  public reset(): void {
+    this._cards = [];
+  }
 
-	public reset(): void {
-		this._cards = [];
-	}
+  public rearrange(): void {
+    this._cards.forEach((card, index) => {
+      card.x = index * 0.2;
+      card.y = -index * 0.2;
+      card.scale.set(1);
+    });
+  }
 
-	public rearrange(): void {
-		this._cards.forEach((card, index) => {
-			card.x = index * 0.2;
-			card.y = -index * 0.2;
-			card.scale.set(1);
-		})
-	}
-
-	public getTopCard(): Card {
-		return this._cards.pop();
-	}
+  public getTopCard(): Card | undefined {
+    return this._cards.pop();
+  }
 }
